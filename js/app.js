9a5fd92958041941314ccfbe2d7d181599cdff58
pryntl <pryntl2007@gmail.com>
2024-12-13 22:38:19 +0330

    feat js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
-function getBudget(){
-  prompt("enter the budjet")
+function getBudget(e) {
+  e.preventDefault;
+  let budget = prompt("enter the budjet");
+
+  if (budget !== "") {
+    document.querySelector("#total").textContent = budget;
+    document.querySelector("#left").textContent = budget;
+  } else {
+    while (budget == "") {
+      budget = prompt("enter the budjet");
+      if (budget !== "") {
+        document.querySelector("#total").textContent = budget;
+        document.querySelector("#left").textContent = budget;
+      }
+    }
+  }
 }
-window.onload = getBudget
+window.onload = getBudget;
+const btn = document.querySelector(".btn-primary");
+btn.addEventListener("click", getExpense);
+function getExpense(e) {
+  e.preventDefault();
+  const expense = document.querySelector("#expense").value;
+  const amount = document.querySelector("#amount").value;
+}
