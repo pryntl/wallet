d997ff6bf07b4a9d1492c71fa48a62416073465a
pryntl <pryntl2007@gmail.com>
2024-12-14 21:18:05 +0330

    feat js

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@ function getBudget(e) {
   e.preventDefault;
   let budget = prompt("enter the budjet");
 
-  if (budget !== "") {
+  if (budget !== "" && !isNaN(budget)) {
     document.querySelector("#total").textContent = budget;
     document.querySelector("#left").textContent = budget;
   } else {
-    while (budget == "") {
-      budget = prompt("enter the budjet");
-      if (budget !== "") {
+    while (budget == "" || isNaN(budget)) {
+      budget = prompt("enter the budjet (as number)");
+      if (budget !== "" && !isNaN(budget)) {
         document.querySelector("#total").textContent = budget;
         document.querySelector("#left").textContent = budget;
       }
@@ -20,6 +20,17 @@ const btn = document.querySelector(".btn-primary");
 btn.addEventListener("click", getExpense);
 function getExpense(e) {
   e.preventDefault();
-  const expense = document.querySelector("#expense").value;
-  const amount = document.querySelector("#amount").value;
+  const expense = document.querySelector("#expense");
+  const amount = document.querySelector("#amount");
+  const ul = document.querySelector('.list-group');
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  const span2 = document.createElement('span');
+  span.textContent = expense.value;
+  span2.textContent = amount.value;
+  li.appendChild(span);
+  li.appendChild(span2);
+  ul.appendChild(li);
+  expense.value = '';
+  amount.value = '';
 }
